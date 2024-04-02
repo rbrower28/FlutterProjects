188d43c1a024245a88b4c77be87af13f933f172c
Ryan Brower <ryan.brower28@gmail.com>
2024-04-02 00:55:46 -0600

    Dice app

---
+++ dice_roller/lib/dice_roller.dart
@@ -0,0 +1,47 @@
+import "dart:math";
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State {
+  final randomizer = Random();
+  int currentDiceRoll = 2;
+
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1; // 1 - 6
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          "assets/images/dice-$currentDiceRoll.png",
+          width: 200,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(
+                fontSize: 28,
+              )),
+          child: const Text("Roll Dice"),
+        ),
+      ],
+    );
+  }
+}
